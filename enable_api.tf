dd89b290bd6251cde4b71ca7b0931c051739d130
Sean Alvarez <alvie0818@gmail.com>
2023-07-26 12:43:23 -0400

    Fix build step

--- enable_api.tf
+++ enable_api.tf
@@ -1,24 +1,29 @@
 resource "google_project_service" "iam_credentials" {
   project = var.gcp_project_id
   service = "iamcredentials.googleapis.com"
+  disable_dependent_services=true
 }
 
 resource "google_project_service" "iam" {
   project = var.gcp_project_id
   service = "iam.googleapis.com"
+  disable_dependent_services=true
 }
 
 resource "google_project_service" "crm" {
   project = var.gcp_project_id
   service = "cloudresourcemanager.googleapis.com"
+  disable_dependent_services=true
 }
 
 resource "google_project_service" "service_usage" {
   project = var.gcp_project_id
   service = "serviceusage.googleapis.com"
+  disable_dependent_services=true
 }
 
 resource "google_project_service" "compute" {
   project = var.gcp_project_id
   service = "compute.googleapis.com"
+  disable_dependent_services=true
 }
