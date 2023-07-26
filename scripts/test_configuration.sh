84b486de41a54b84220992d0592690cd6f0b60b2
Sean Alvarez <alvie0818@gmail.com>
2023-07-26 16:53:32 -0400

    Filter test log output

--- scripts/test_configuration.sh
+++ scripts/test_configuration.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 export ENVIRONMENT=$1
 
-gcloud auth login --cred-file=./gcp-creds.json
-gcloud config set auth/impersonate_service_account empc-iam-profiles-sa@${GCP_PROJECT_ID}.iam.gserviceaccount.com
+gcloud auth login --cred-file=./gcp-creds.json &>/dev/null
+gcloud config set auth/impersonate_service_account empc-iam-profiles-sa@${GCP_PROJECT_ID}.iam.gserviceaccount.com &>/dev/null
 
 inspec exec tests/lab-gcp-iam-profiles -t gcp:// --input gcp_project_id=${GCP_PROJECT_ID}
 
 #Unset impersonation (mostly for local testing and caches)
-gcloud config unset auth/impersonate_service_account
+gcloud config unset auth/impersonate_service_account &>/dev/null
