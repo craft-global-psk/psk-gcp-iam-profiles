358ca1ba50ddfc1e7fe14afeb3fe979937ffa812
Sean Alvarez <alvie0818@gmail.com>
2023-10-19 16:39:59 -0400

    Script gcloud auth for CircleCi Pipeline
    Signed-off-by: Sean Alvarez <alvie0818@gmail.com>

---
+++ scripts/gcp_authenticate.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+OP_SECRET=psk-platform-gcp-workload-identity-config
+WIP_ID=$(op item get $OP_SECRET --vault empc-lab --fields label=workload-identity-pool-id)
+SERVICE_ACCOUNT_EMAIL=$(op item get $OP_SECRET --vault empc-lab --fields label=service-account-email)
+
+OIDC_TOKEN_PATH=$1
+GCP_CRED_FILE_PATH=$2
+
+# Store OIDC token in temp file
+echo $CIRCLE_OIDC_TOKEN > $OIDC_TOKEN_PATH
+
+# Create a credential configuration for the generated OIDC ID Token
+gcloud iam workload-identity-pools create-cred-config \
+    $WIP_ID \
+    --output-file=$GCP_CRED_FILE_PATH \
+    --service-account=$SERVICE_ACCOUNT_EMAIL \
+    --credential-source-file=$OIDC_TOKEN_PATH
+
+# Configure gcloud to leverage the generated credential configuration
+gcloud auth login --brief --cred-file $GCP_CRED_FILE_PATH
+# Configure ADC
+echo "export GOOGLE_APPLICATION_CREDENTIALS='$GCP_CRED_FILE_PATH'" | tee -a "$BASH_ENV"
