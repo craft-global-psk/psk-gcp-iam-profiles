31e6df7e116becf0be6baaa9f1cc292e494aad56
Sean Alvarez <alvie0818@gmail.com>
2023-10-19 15:56:32 -0400

    Script to store workload identity pool values to 1Password
    Signed-off-by: Sean Alvarez <alvie0818@gmail.com>

--- scripts/store_gcp_identity_config.sh
+++ scripts/store_gcp_identity_config.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-echo "Store workload itentity pool values"
-exec 1> /dev/null
+echo "Store workload identity pool values ..."
+
+{
 ITEM_NAME=psk-platform-gcp-workload-identity-config
 
 # Create the 1Password item if it doesn't exist
@@ -17,8 +18,12 @@ fi
 
 # Retrieve the workload identity config values
 terraform init
-WIP_ID=$(terraform output workload_identity_configuration_id)
-SERVICE_ACCOUNT_EMAIL=$(terraform output service_account_email)
+WIP_ID=$(terraform output workload_identity_configuration_id | sed 's/"//g')
+SERVICE_ACCOUNT_EMAIL=$(terraform output service_account_email | sed 's/"//g')
 
 # write the workload identity config values to the secret store
 op item edit "$ITEM_NAME" --account="twdps" --vault="empc-lab" "workload-identitty-pool-id[text]=$WIP_ID" "service-account-email[text]=$SERVICE_ACCOUNT_EMAIL"
+} >/dev/null
+
+echo "Values stored"
+echo ""
